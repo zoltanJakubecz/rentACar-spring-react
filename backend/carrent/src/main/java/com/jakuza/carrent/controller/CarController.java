22b3b54d306ada5bcd774034128224d4e6506aa5
Zoltan Jakubecz <jakuzo@gmail.com>
2020-08-04 14:25:01 +0200

    Add get one car endpoint

--- backend/carrent/src/main/java/com/jakuza/carrent/controller/CarController.java
+++ backend/carrent/src/main/java/com/jakuza/carrent/controller/CarController.java
@@ -8,6 +8,7 @@ import com.jakuza.carrent.service.CarService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +30,10 @@ public class CarController {
     @PostMapping
     public Car addCar(@RequestBody CarDto car){
         return carService.addCar(car);
+    }
+
+    @GetMapping("/{carId}")
+    public Car getCar(@PathVariable Long carId){
+        return carService.getCar(carId);
     }
 }
